ba61b2381ce5d7f8be94ce3f1da436a664e14d2e
xcp <m18770074875_1@163.com>
2018-08-21 22:01:45 +0800

    x

--- src/ave/A360/A360.js
+++ src/ave/A360/A360.js
@@ -4,7 +4,53 @@
  * 360广告本身上报及联盟上报
  * 本库完全依赖于jQuery
  */
+//getBoundingClientRect().top < $(window).height();
 import	'./A360.less';
+
+const inview = ($cnt,callback) => {
+	const lock = false;
+	const win = $(window).height();
+	const isview = () => {
+		if($cnt.length === 0)return;
+		win = $(window).height();
+		let rect = $cnt[0].getBoundingClientRect();
+		if(rect.top > 0 && rect.top < win){
+			$(document).unbind('scroll',isview);
+		}
+	}
+	$(document).on('scroll',isview);
+	isview();
+}
+
+const showurl = (k) => {
+	$.each(k,(i,item)=>{
+		new Image().src = item.replace(/^(http:\/\/)|(https:\/\/)/i,'')
+	})
+}
+
+const viewurl = ($cnt,k) => {
+	inview($cnt,()=>{
+		
+	})
+}
+showurl:function(k){
+                $.each(k, function(i, item) {
+                    new Image().src = item.replace("http:", "");
+                });
+            },
+            viewurl:function($dom,k){
+                var self = this ;
+                self.inview($dom,function(){
+                    self.showurl(k);
+                }) 
+            },
+            clickurl:function(urlobj) {
+                var self = this ;
+                var clktkurlArry = $.extend(true,[],urlobj.clktk);
+                $.each(clktkurlArry, function(i,item) {
+                    $('body').append('<iframe src="' + self.replace360url(item,urlobj) + '" style="display: none;"></iframe>');
+                });
+            },
 
 export default {
 	'xcp':'hahah'
